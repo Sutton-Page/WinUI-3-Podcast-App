b5cf9f8800223a7ac59dfc3130d8d734e157f5a0
LazyOverLord <44475186+Sutton-Page@users.noreply.github.com>
2024-05-14 22:27:29 -0400

    Added class to represent podcast item

---
+++ NativeLearning/PodcastItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeLearning
+{
+    internal class PodcastItem
+    {
+
+        public String title { get; set; }
+        public String description { get; set; }
+
+        public String url { get; set; }
+
+
+        public PodcastItem(string title, string description, string url, )
+        {
+            this.title = title;
+            this.description = description;
+            this.url = url;
+        }
+
+
+    }
+}
