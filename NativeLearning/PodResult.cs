0bbc0b2c0a6ed07363a78a799d099844de2b1472
LazyOverLord <44475186+Sutton-Page@users.noreply.github.com>
2024-05-16 17:10:13 -0400

    Added class to add search object and parse json

---
+++ NativeLearning/PodResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeLearning
+{
+    public class PodResult
+    {
+        public String name {  get; set; }
+        public String imageUrl { get; set; }
+
+        public String feedUrl { get; set; }
+
+        public PodResult(String name, String imageUrl, String feedUrl) {
+
+            this.name = name;
+            this.imageUrl = imageUrl;
+            this.feedUrl = feedUrl;
+        
+        }
+    }
+}
