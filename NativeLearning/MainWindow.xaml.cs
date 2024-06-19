012d9d397fe73629d7e9bfb6ecdb7988c83a10be
LazyOverLord <44475186+Sutton-Page@users.noreply.github.com>
2024-06-19 15:36:59 -0400

    basic navView setup

--- NativeLearning/MainWindow.xaml.cs
+++ NativeLearning/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,7 @@ using System.Text.Json.Serialization;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
+using Windows.UI.ApplicationSettings;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,79 +30,112 @@ namespace NativeLearning
 
     public sealed partial class MainWindow : Window
     {
-        private bool _clicked;
-        public PodcastViewModel ViewModel {get; set; }
 
-        Windows.Storage.StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
 
         public MainWindow()
         {
             this.InitializeComponent();
-            this.ViewModel = new PodcastViewModel();
-
-            setupConfig();
-            setupApp();
 
 
-            
-        }
-
-        private async void setupConfig()
-        {
-            try
-            {
 
 
-                CStorage config = new CStorage();
 
-                var file = await ApplicationData.Current.LocalFolder.CreateFileAsync("config.json", CreationCollisionOption.FailIfExists);
-
-                var handle = await file.OpenStreamForWriteAsync();
-
-                await JsonSerializer.SerializeAsync(handle, config);
-
-            }
-
-            catch(System.Exception ex)
-            {
-
-            }
 
         }
 
-        private void setupApp()
-        {
-
-            nav.Navigate(typeof(Home));
-        }
-
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
-        
-        
-
-
-        /*
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
         {
-            // myButton.Content = "Clicked";
+            
+            
 
-            if (_clicked)
+            // Add handler for ContentFrame navigation.
+            ContentFrame.Navigated += On_Navigated;
+
+            // NavView doesn't load any page by default, so load home page.
+            NavView.SelectedItem = NavView.MenuItems[0];
+            // If navigation occurs on SelectionChanged, this isn't needed.
+            // Because we use ItemInvoked to navigate, we need to call Navigate
+            // here to load the home page.
+            //NavView_Navigate(typeof(HomePage), new EntranceNavigationTransitionInfo());
+
+            NavView_Navigate(typeof(Home), new EntranceNavigationTransitionInfo());
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender,
+                                         NavigationViewItemInvokedEventArgs args)
+        {
+            
+             if (args.InvokedItemContainer != null)
             {
-                myButton.Content = "Clicked";
-
+                Type navPageType = Type.GetType(args.InvokedItemContainer.Tag.ToString());
+                NavView_Navigate(navPageType, args.RecommendedNavigationTransitionInfo);
             }
-            else
+        }
+
+        
+
+        private void NavView_Navigate(Type navPageType, NavigationTransitionInfo transitionInfo)
+        {
+            // Get the page type before navigation so you can prevent duplicate
+            // entries in the backstack.
+            Type preNavPageType = ContentFrame.CurrentSourcePageType;
+
+            // Only navigate if the selected page isn't currently loaded.
+            if (navPageType is not null && !Type.Equals(preNavPageType, navPageType))
             {
-                myButton.Content = "Click Me";
+                ContentFrame.Navigate(navPageType, null, transitionInfo);
             }
+        }
 
-            this._clicked = !this._clicked;
+        private void NavView_BackRequested(NavigationView sender,
+                                           NavigationViewBackRequestedEventArgs args)
+        {
+            TryGoBack();
+        }
+
+        private bool TryGoBack()
+        {
+            if (!ContentFrame.CanGoBack)
+                return false;
+
+            // Don't go back if the nav pane is overlayed.
+            if (NavView.IsPaneOpen &&
+                (NavView.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+
+            ContentFrame.GoBack();
+            return true;
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = ContentFrame.CanGoBack;
+
+           
+           /*
+             if (ContentFrame.SourcePageType != null)
+            {
+                // Select the nav view item that corresponds to the page being navigated to.
+                NavView.SelectedItem = NavView.MenuItems
+                            .OfType<NavigationViewItem>()
+                            .First(i => i.Tag.Equals(ContentFrame.SourcePageType.FullName.ToString()));
+
+                NavView.Header =
+                    ((NavigationViewItem)NavView.SelectedItem)?.Content?.ToString();
+
+            }*/
+        }
 
 
-        }*/
+
+
+
     }
 }
